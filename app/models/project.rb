204e9d71f6f1d9b5835ad8c7cdbf057fd725839d
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2021-04-12 09:27:44 -0700

    Added Complete Edit button to task.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
     has_many :resources, dependent: :delete_all
     has_many :technologies, through: :resources
     belongs_to :developer, optional: true
-    has_many :tasks
+    has_many :tasks, dependent: :delete_all
 
     validates :title, presence: true
     validates :description, presence: true
